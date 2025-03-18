94f6b9119d5584ca3c94d794000b0b960e2a2d5b
sonhv0212 <son.ho@skymavis.com>
2025-03-18 12:51:52 +0700

    params: remove PragueBlock from TestChainConfig

--- params/config.go
+++ params/config.go
@@ -469,7 +469,6 @@ var (
 		BerlinBlock:                   big.NewInt(0),
 		LondonBlock:                   big.NewInt(0),
 		CancunBlock:                   big.NewInt(0),
-		PragueBlock:                   big.NewInt(0),
 		ArrowGlacierBlock:             nil,
 		OdysseusBlock:                 nil,
 		FenixBlock:                    nil,
