551894e71162c35b808be37863a1a4769b00dfb7
Tran Tien Duc <trantienduchn@gmail.com>
2025-03-26 18:17:17 +0700

    fixup! core/state: fix SetStorage override behavior (#30185)

--- eth/tracers/api_test.go
+++ eth/tracers/api_test.go
@@ -1021,8 +1021,9 @@ func TestTracingWithOverrides(t *testing.T) {
 					},
 				},
 			},
-			want: `{"gas":46900,"failed":false,"returnValue":"0000000000000000000000000000000000000000000000000000000000000539"}`,
-			//want: `{"gas":44100,"failed":false,"returnValue":"0000000000000000000000000000000000000000000000000000000000000001"}`,
+			//want: `{"gas":46900,"failed":false,"returnValue":"0000000000000000000000000000000000000000000000000000000000000539"}`,
+			// fakeStorage side effect
+			want: `{"gas":44100,"failed":false,"returnValue":"0000000000000000000000000000000000000000000000000000000000000001"}`,
 		},
 		{ // No state override
 			blockNumber: rpc.LatestBlockNumber,
