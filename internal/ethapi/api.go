b14e83769f1fba42c972aa4cb1eb92f2a4751f4e
Tran Tien Duc <trantienduchn@gmail.com>
2025-11-06 23:01:43 +0700

    internal/ethapi: reject new tx on L2 block

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2042,6 +2042,9 @@ func (s *PublicTransactionPoolAPI) sign(addr common.Address, tx *types.Transacti
 
 // SubmitTransaction is a helper function that submits tx to txPool and logs a message.
 func SubmitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (common.Hash, error) {
+	if b.ChainConfig().IsL2Migration(b.CurrentHeader().Number) {
+		return common.Hash{}, errors.New("transactions will not be executed on L1 anymore, please switch to L2")
+	}
 	// If the transaction fee cap is already specified, ensure the
 	// fee of the given transaction is _reasonable_.
 	if err := checkTxFee(tx.GasPrice(), tx.Gas(), b.RPCTxFeeCap()); err != nil {
